a2bb3148226c8c99c0c274091cf5c7e2da1175a2
Vitaliy <skrepach@gmail.com>
2022-04-23 13:51:39 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+# a, b = map(float, input().split())
+from pathlib import Path
+def print_hi():
+    # a1, a2, a3, a4, a5, a6 = list(map(int, input().split()))
+    # word_list = list(map(int, input().split()))
+    #n = list(map(int,input()
+    lst_in = ['Муму', 'Евгений Онегин', 'Сияние', 'Мастер и Маргарита', 'Пиковая дама', 'Колобок']
+    i = 0
+    rez = []
+    while i < len(lst_in):
+        if len(lst_in[i].split(" ")) < 2:
+            rez.append(lst_in[i])
+            rez.append("\n")
+        i +=1
+
+    joined_string = "".join(rez)
+    print(joined_string)
+
+
+# create a new director
+def add_new_dir():
+    filepath = Path("temp/test.txt")
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+
+def get_dir():
+    home = Path.home()
+    return home
+
+def create_file(): # каждый раз новый при вызове метода?
+    myfile = Path(Path.home(), "file.txt")
+    myfile.touch(exist_ok=True)
+    f = open(myfile)
+
+def get_list_dir():
+    p = Path('.')
+    return [x for x in p.iterdir() if x.is_dir()]
+
+if __name__ == '__main__':
+    print_hi()
